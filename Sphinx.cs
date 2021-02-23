f56ace4c4f4a7f6142efc4992d2a49497a2620f7
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-02-22 18:50:43 -0800

    Implements foreach loop, add failure on incorrect answer, add dictionary, utilize Random
    Co-authored-by: Ash Porter <porter.ashley13@gmail.com>

--- Sphinx.cs
+++ Sphinx.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic; // Might not need this if we don't use a dictionary
+using System.Linq;
 
 public class Riddle
 {
@@ -11,69 +12,38 @@ public class Riddle
     Question = question;
     Answer = answer;
   }
-}
 
+  public bool AnswerIsCorrect(string answer, string userInput) {
+    if (userInput == answer) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
 
 public class Program
 {
   public static void Main()
   {
-    Riddle one = new Riddle("Question 1: What goes on four feet in the morning, two feet at noon, and three feet in the evening?", "person");
-    Riddle two = new Riddle("Question 2: I am not alive, but I grow; I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?.", "fire");
-    Riddle three = new Riddle("What kind of ear cannot hear?", "an ear of corn");
-    Riddle four = new Riddle("You must keep it after giving it.", "your word");
+    Dictionary<string, string> prompts = new Dictionary<string, string>() { {"What goes on four feet in the morning, two feet at noon, and three feet in the evening?", "person"}, {"I am not alive, but I grow; I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?", "fire"}, {"What kind of ear cannot hear?", "an ear of corn"}, {"You must keep it after giving it.", "your word"} };
 
-    Console.WriteLine("Hello, I am the Sphinx. To pass my test, you must correctly answer my riddles. If you fail, you are eaten.");
-    Console.WriteLine(one.Question);
-    string answer = Console.ReadLine().ToLower();
-    if (answer == one.Answer) {
-      Console.WriteLine("Correct.");
-    } else {
-      Console.WriteLine("Wrong. You will now be eaten.");
+    bool correct = true;
+    while(correct) {
+      Random rand = new Random();
+      prompts = prompts.OrderBy(x => rand.Next())
+        .ToDictionary(item => item.Key, item => item.Value);
+
+      Console.WriteLine("I am Sphinx hear me roar");
+      int index = rand.Next(prompts.Count);
+
+      KeyValuePair<string, string> pair = prompts.ElementAt(index);
+
+      Console.WriteLine(pair.Key);
+      string response = Console.ReadLine().ToLower();
+      if (response != pair.Value) {
+        correct = false;
+      }
     }
   }
 }
-
-// {
-//   static void Main()
-//   {
-    // List<Car> Cars = new List<Car>() { volkswagen, yugo, ford, amc };
-    // string[] questions = new string["", "", "", ""];
-    // string[] answers = new string["", "", "", ""];
-    
-    // Dictionary option
-    // If userInput = answerKey[1])
-
-    // random number generator = randomNumber(4)
-    // Console.WriteLine(questions[randomNumber])
-    // Console.ReadLine()
-    // if (readline == answer[randomNumber]) {
-        // Correct response
-        // if user wants to continue: y or n
-        // if (y) {
-          // run the main function again
-          // possible validation to make sure we don't do the same question again
-          // end case when all the questions are gone
-        // } else quit
-    // } else get eaten
-
-//     Dictionary<string, string> riddles = new Dictionary<string, int>() { {: "person"}, {: "fire"}, {"What kind of ear cannot hear?": "An ear of corn"}, {"You must keep it after giving it.": "Your word"} };
-//     string response = Console.ReadLine().ToLower();
-//     if (response.Contains(riddles[""])) {
-
-
-//     // Console.WriteLine("Question 1: What goes on four feet in the morning, two feet at noon, and three feet in the evening?");
-//     // string answer1 = "person";
-//       Console.WriteLine("Correct.");
-//       Console.WriteLine("Question 2: I am not alive, but I grow; I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?.");
-//       bool response2 = Console.ReadLine().ToLower().Contains("fire");
-//       if (response2) {
-//         Console.WriteLine("Correct.");
-//       } else {
-//         Console.WriteLine("Wrong. You will now be eaten.");
-//       }
-//     } else {
-//       Console.WriteLine("Wrong. You will now be eaten.");
-//     }
-//   }
-// }
